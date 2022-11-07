43ef2fb782651ea08f598663e1227ecebbcdc5ee
Hana <ortizsanchezhana@gmail.com>
2022-11-07 13:32:12 -0500

    main

---
+++ jsLessons/test.js
@@ -0,0 +1,43 @@
+<!--var myName = "Hana"
+
+document.write(myName);
+
+console.log(myName)
+
+
+
+
+
+
+
+
+
+
+for (i = 0; i < 10; i++) {
+    
+ 
+    if (i === 5 || i === 3) {
+        
+        continue;
+    }
+ 
+    console.log(i);
+ 
+    if(i === 7) {
+        console.log("The Loop is broken")
+        break; 
+        
+                }
+   
+                        }
+
+                        var links = document.getElementsByTagName('a');
+ 
+for (i = 0; i < links.length; i++){
+    
+}
+
+var links = document.getElementsByTagName('a');
+ 
+for (i = 0; i < links.length; i++){
+    links[i].className = "Link " + i;
